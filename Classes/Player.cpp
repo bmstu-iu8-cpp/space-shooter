f21002a0ed390f23deaa56cda35a23d55ee9d8d3
drewxa <bo.ro.da@mail.ru>
2018-02-26 01:33:57 +0300

    nullref exceptoion

--- Classes/Player.cpp
+++ Classes/Player.cpp
@@ -5,13 +5,13 @@ Player::Player(float x, float y, const std::string & filenamePicture)
     , X(x)
     , Y(y)
 {
-    if (View == nullptr)
-        throw std::runtime_error("can't create a sprite");
-
     View->setPosition(X, Y);
 }
 
 cocos2d::Sprite * Player::Sprite()
 {
+    if (View == nullptr)
+        throw std::runtime_error("sprite was not been loaded");
+
     return View;
 }
